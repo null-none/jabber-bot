c71eb881dbca8dda17e6e7b3fa71738145545755
Dmitry Kalinin <dmitry.kalinin.email@gmail.com>
2016-09-26 20:24:23 +0300

    0.1.1

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='jabber-bot',
-    version='0.0.1',
+    version='0.1.1',
     packages=find_packages(),
     author='Dmitry Kalinin',
     author_email='dmitry.kalinin.email@gmail.com',
